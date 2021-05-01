4b1a95391e15c230a2614f96507d3dbb5811fa27
christopherohit <55018999+christopherohit@users.noreply.github.com>
2021-05-01 20:15:02 +0700

    Create MainDAO

---
+++ DAO/MainDAO.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Raven.DAO
+{
+    public class MainDAO
+    {
+        private static MainDAO instance;
+
+        public static MainDAO Instance 
+        {
+            get { if (instance == null) instance = new MainDAO(); return instance; }
+            private set => instance = value;
+        }
+        private MainDAO() { }
+    }
+}
