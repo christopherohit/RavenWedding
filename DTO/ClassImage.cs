b28a90d22c5929bfcb77c0e5cf2d9a119a8062d6
christopherohit <55018999+christopherohit@users.noreply.github.com>
2021-05-04 19:00:51 +0700

    Create Shortcut to make everything easy than (Have 4 Error which I'm Trying Fixing)

---
+++ DTO/ClassImage.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Raven.DTO
+{
+    public class ClassImage
+    {
+        string PlaceSave = Path.GetFullPath(@"D:\");
+        public string SaveImage(string name)
+        {
+            int i = 1;
+            try
+            {
+                Directory.CreateDirectory(PlaceSave + @"Lesson\RIT\C #\Winform\Raven\Database\User_Data\" + name);
+                if (Directory.Exists(PlaceSave + @"Lesson\RIT\C #\Winform\Raven\Database\User_Data\" + name))
+                {
+                sudunglai:
+                    i++;
+                    Directory.CreateDirectory(PlaceSave + @"Lesson\RIT\C #\Winform\Raven\Database\User_Data\" + name + i);
+                    if (Directory.Exists(PlaceSave + @"Lesson\RIT\C #\Winform\Raven\Database\User_Data\" + name + i))
+                    {
+                        goto sudunglai;
+                    }
+                    else
+                    {
+                        string SetFull = PlaceSave + @"Lesson\RIT\C #\Winform\Raven\Database\User_Data\" + name + i;
+                        return SetFull;
+                    }
+                }
+            }
+            catch(Exception er)
+            {
+
+            }
+        }
+    }
+}
