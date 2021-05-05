8d3dcba713c12a53d76d46f62f88b4321cf86ccc
christopherohit <55018999+christopherohit@users.noreply.github.com>
2021-05-05 22:53:54 +0700

    Add Block When go wrong more 4 times

--- DTO/ClassImage.cs
+++ DTO/ClassImage.cs
@@ -10,32 +10,5 @@ namespace Raven.DTO
     public class ClassImage
     {
         string PlaceSave = Path.GetFullPath(@"D:\");
-        public string SaveImage(string name)
-        {
-            int i = 1;
-            try
-            {
-                Directory.CreateDirectory(PlaceSave + @"Lesson\RIT\C #\Winform\Raven\Database\User_Data\" + name);
-                if (Directory.Exists(PlaceSave + @"Lesson\RIT\C #\Winform\Raven\Database\User_Data\" + name))
-                {
-                sudunglai:
-                    i++;
-                    Directory.CreateDirectory(PlaceSave + @"Lesson\RIT\C #\Winform\Raven\Database\User_Data\" + name + i);
-                    if (Directory.Exists(PlaceSave + @"Lesson\RIT\C #\Winform\Raven\Database\User_Data\" + name + i))
-                    {
-                        goto sudunglai;
-                    }
-                    else
-                    {
-                        string SetFull = PlaceSave + @"Lesson\RIT\C #\Winform\Raven\Database\User_Data\" + name + i;
-                        return SetFull;
-                    }
-                }
-            }
-            catch(Exception er)
-            {
-
-            }
-        }
     }
 }
