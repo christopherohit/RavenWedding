901f8e8b543522f9afd0c33c9b5acc7be1154bc3
christopherohit <55018999+christopherohit@users.noreply.github.com>
2021-05-07 18:50:43 +0700

    Add and Update for 3 Method for update , Block , Insert User

--- DAO/AccountDAO.cs
+++ DAO/AccountDAO.cs
@@ -47,12 +47,12 @@ namespace Raven.DAO
             return ICM;
 
         }
-        public bool GetPhone(string username)
+        public bool GetPhone(string Phone)
         {
-            string getPhone = "Select Phone from nhanvien where Email = N'" + username + "'";
+            string getPhone = "Select Phone from nhanvien where Phone = N'" + Phone + "'";
             DataTable exportPhone = DataProvider.Instance.ExecuteQuery(getPhone);
             return  exportPhone.Rows.Count > 0;
-        }
+        }  
         public string Getame(string username)
         {
             string getName = "select * from nhanvien where Email = N'" + username + "'";
@@ -69,15 +69,30 @@ namespace Raven.DAO
             var stream = new MemoryStream(Image);
             return stream.ToString() ;
         }
-        public bool ICMCheck(string username)
+        public bool ICMCheck(string ICM)
         {
-            string checkICM = "select cmnd from nhanvien where Email = N'" + username + "'";
+            string checkICM = "select cmnd from nhanvien where Email = N'" + ICM + "'";
             DataTable exportICM = DataProvider.Instance.ExecuteQuery(checkICM);
             return exportICM.Rows.Count > 0;
         }
         public bool ChangePass(string Username , string password)
         {
             int result = DataProvider.Instance.ExecuteNonQuery("EXEC CapNhapPass @Emails , @pass", new object[] { Username, password });
+            return result > 0;
+        }
+        public bool UpdateInfo(string username , string gender , string phone , string address , string icm , DateTime DOB , string Des)
+        {
+            int result = DataProvider.Instance.ExecuteNonQuery("EXEC FillIn4InFormation @Emails  , @gender =  , @phone =  , @Address =  , @ICM =  , @DOB =  , @DES =  ", new object[] { username , gender , phone , address , icm , DOB , Des });
+            return result > 0;
+        }
+        public bool Block(string username, bool block)
+        {
+            int Result = DataProvider.Instance.ExecuteNonQuery("EXEC BlockAccount @Emails , @isblock", new object[] { username, block });
+            return Result > 0;
+        }
+        public bool InsertUser (string fullname , string mails , string pass)
+        {
+            int result = DataProvider.Instance.ExecuteNonQuery("EXEC InsertMember @name  , @email  , @pass ", new object[] { fullname, mails, pass });
             return result > 0;
         }
     }
