8f3a698c1afe638e2355bfb2a0d782b9cf059601
christopherohit <55018999+christopherohit@users.noreply.github.com>
2021-05-14 15:20:06 +0700

    Unknow

--- Properties/Settings.Designer.cs
+++ Properties/Settings.Designer.cs
@@ -33,5 +33,16 @@ namespace Raven.Properties {
                 return ((string)(this["OnYourWeddingDayConnectionString"]));
             }
         }
+        
+        [global::System.Configuration.ApplicationScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.SpecialSettingAttribute(global::System.Configuration.SpecialSetting.ConnectionString)]
+        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=DESKTOP-F970216;Initial Catalog=OnYourWeddingDay;Integrated Security=" +
+            "True")]
+        public string OnYourWeddingDayConnectionString1 {
+            get {
+                return ((string)(this["OnYourWeddingDayConnectionString1"]));
+            }
+        }
     }
 }
