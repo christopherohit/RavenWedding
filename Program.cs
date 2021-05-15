8ee68f4e1b16afb416905522418cbcbb8fb4943e
christopherohit <55018999+christopherohit@users.noreply.github.com>
2021-05-15 10:56:16 +0700

    Programs Empty Error 15%

--- Program.cs
+++ Program.cs
@@ -16,7 +16,7 @@ namespace Raven
         {   
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Profile());
+            Application.Run(new SignIn());
         }
     }
 }
