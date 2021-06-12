2e6d707b5ed8b5ccf34adafef56b9dcc365eccfe
christopherohit <55018999+christopherohit@users.noreply.github.com>
2021-06-12 22:26:05 +0700

    Hint of Textbox in SignIn Form

---
+++ Package/Hints.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Raven.Package
+{
+    public partial class PHTextBox : Bunifu.Framework.UI.BunifuMaterialTextbox
+    {
+        System.Drawing.Color DefaultColor;
+        public string PlaceHolderText { get; set; }
+        public PHTextBox(string placeholdertext)
+        {
+            // get default color of text
+            DefaultColor = this.ForeColor;
+            // Add event handler for when the control gets focus
+            this.GotFocus += (object sender, EventArgs e) =>
+            {
+                this.Text = String.Empty;
+                this.ForeColor = DefaultColor;
+            };
+
+            // add event handling when focus is lost
+            this.LostFocus += (Object sender, EventArgs e) => {
+                if (String.IsNullOrEmpty(this.Text) || this.Text == PlaceHolderText)
+                {
+                    this.ForeColor = System.Drawing.Color.Gray;
+                    this.Text = PlaceHolderText;
+                }
+                else
+                {
+                    this.ForeColor = DefaultColor;
+                }
+            };
+
+
+
+            if (!string.IsNullOrEmpty(placeholdertext))
+            {
+                // change style   
+                this.ForeColor = System.Drawing.Color.Gray;
+                // Add text
+                PlaceHolderText = placeholdertext;
+                this.Text = placeholdertext;
+            }
+
+
+
+        }
+
+
+    }
+}
