b4704cbf17cce159a8c6fe398c758742e514aa13
christopherohit <55018999+christopherohit@users.noreply.github.com>
2021-06-26 04:00:20 +0700

    Designing form create task , project you can add people into this project moreover you can add the event and Schedule

---
+++ TaskForm.Designer.cs
@@ -0,0 +1,86 @@
+﻿
+namespace Raven
+{
+    partial class TaskForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.bunifuElipse1 = new Bunifu.Framework.UI.BunifuElipse(this.components);
+            this.behaviorManager1 = new DevExpress.Utils.Behaviors.BehaviorManager(this.components);
+            this.pagerEvents1 = new DevExpress.Utils.Behaviors.Common.PagerEvents(this.components);
+            this.svgImageCollection1 = new DevExpress.Utils.SvgImageCollection(this.components);
+            this.radioGroup1 = new DevExpress.XtraEditors.RadioGroup();
+            ((System.ComponentModel.ISupportInitialize)(this.behaviorManager1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.svgImageCollection1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.radioGroup1.Properties)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // bunifuElipse1
+            // 
+            this.bunifuElipse1.ElipseRadius = 5;
+            this.bunifuElipse1.TargetControl = this;
+            // 
+            // pagerEvents1
+            // 
+            this.pagerEvents1.CustomizePagerItem += new System.EventHandler<DevExpress.Utils.CustomizePagerItemEventArgs>(this.pagerEvents1_CustomizePagerItem_1);
+            // 
+            // radioGroup1
+            // 
+            this.radioGroup1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.radioGroup1.Location = new System.Drawing.Point(0, 0);
+            this.radioGroup1.Name = "radioGroup1";
+            this.radioGroup1.Size = new System.Drawing.Size(1013, 96);
+            this.radioGroup1.TabIndex = 0;
+            this.radioGroup1.SelectedIndexChanged += new System.EventHandler(this.radioGroup1_SelectedIndexChanged);
+            // 
+            // TaskForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(1013, 526);
+            this.Controls.Add(this.radioGroup1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "TaskForm";
+            this.Text = "TaskForm";
+            ((System.ComponentModel.ISupportInitialize)(this.behaviorManager1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.svgImageCollection1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.radioGroup1.Properties)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Bunifu.Framework.UI.BunifuElipse bunifuElipse1;
+        private DevExpress.Utils.Behaviors.BehaviorManager behaviorManager1;
+        private DevExpress.Utils.Behaviors.Common.PagerEvents pagerEvents1;
+        private DevExpress.Utils.SvgImageCollection svgImageCollection1;
+        private DevExpress.XtraEditors.RadioGroup radioGroup1;
+    }
+}
---
+++ TaskForm.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DevExpress.XtraBars.Docking2010;
+using System.Windows.Forms;
+using DevExpress.Utils;
+using DevExpress.XtraBars.Navigation;
+
+namespace Raven
+{
+    public partial class TaskForm : Form
+    {
+        public TaskForm()
+        {
+            InitializeComponent();
+        }
+
+
+
+        private void pagerEvents1_CustomizePagerItem_1(object sender, CustomizePagerItemEventArgs e)
+        {
+            WindowsUIButton btn = e.Item as WindowsUIButton;
+            WindowsUIButtonPanel btnPanel = btn.GetOwner() as WindowsUIButtonPanel;
+            NavigationPage page = e.Page as NavigationPage;
+            if (btn == null || btnPanel == null || page == null)
+            {
+                int index = btnPanel.Buttons.IndexOf(btn);
+                btn.Caption = page.ToString();
+                btn.UseCaption = true;
+                btn.ImageOptions.ImageIndex = index;
+            }
+        }
+
+        private void windowsUIButtonPanelMain_Click(object sender, ButtonEventArgs e)
+        {
+            string tag = ((WindowsUIButton)e.Button).Caption.ToString();
+            switch (tag)
+            {
+                case "Home":
+                    Main ea = new Main();
+                    ea.Show();
+                    this.Hide();
+                    break;
+                case "Task":
+                    
+                    break;
+            }
+        }
+
+        private void radioGroup1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
