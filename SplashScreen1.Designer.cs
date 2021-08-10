b81daf34bfd6c509b0f6376cc8fdc03d6d07ef51
christopherohit <55018999+christopherohit@users.noreply.github.com>
2021-08-10 15:07:30 +0300

    Loading Form Has Design and Active

---
+++ SplashScreen1.Designer.cs
@@ -0,0 +1,120 @@
+﻿
+namespace Raven
+{
+    partial class SplashScreen1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+		private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(SplashScreen1));
+            this.progressBarControl = new DevExpress.XtraEditors.MarqueeProgressBarControl();
+            this.labelCopyright = new DevExpress.XtraEditors.LabelControl();
+            this.labelStatus = new DevExpress.XtraEditors.LabelControl();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.progressBarControl.Properties)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // progressBarControl
+            // 
+            this.progressBarControl.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.progressBarControl.EditValue = 0;
+            this.progressBarControl.Location = new System.Drawing.Point(24, 232);
+            this.progressBarControl.Name = "progressBarControl";
+            this.progressBarControl.Size = new System.Drawing.Size(613, 12);
+            this.progressBarControl.TabIndex = 5;
+            // 
+            // labelCopyright
+            // 
+            this.labelCopyright.BorderStyle = DevExpress.XtraEditors.Controls.BorderStyles.NoBorder;
+            this.labelCopyright.Location = new System.Drawing.Point(24, 287);
+            this.labelCopyright.Name = "labelCopyright";
+            this.labelCopyright.Size = new System.Drawing.Size(47, 13);
+            this.labelCopyright.TabIndex = 6;
+            this.labelCopyright.Text = "Copyright";
+            // 
+            // labelStatus
+            // 
+            this.labelStatus.Location = new System.Drawing.Point(24, 215);
+            this.labelStatus.Margin = new System.Windows.Forms.Padding(3, 3, 3, 1);
+            this.labelStatus.Name = "labelStatus";
+            this.labelStatus.Size = new System.Drawing.Size(50, 13);
+            this.labelStatus.TabIndex = 7;
+            this.labelStatus.Text = "Starting...";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(429, 254);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(231, 94);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 10;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(153, 1);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(367, 206);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 11;
+            this.pictureBox2.TabStop = false;
+            // 
+            // SplashScreen1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(661, 348);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.labelStatus);
+            this.Controls.Add(this.labelCopyright);
+            this.Controls.Add(this.progressBarControl);
+            this.Name = "SplashScreen1";
+            this.Padding = new System.Windows.Forms.Padding(1);
+            this.Text = "SplashScreen1";
+            ((System.ComponentModel.ISupportInitialize)(this.progressBarControl.Properties)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private DevExpress.XtraEditors.MarqueeProgressBarControl progressBarControl;
+        private DevExpress.XtraEditors.LabelControl labelCopyright;
+        private DevExpress.XtraEditors.LabelControl labelStatus;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+    }
+}
