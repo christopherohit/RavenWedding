4869f7b62107209f0478c3f4733ef307f99d2273
christopherohit <55018999+christopherohit@users.noreply.github.com>
2021-08-10 15:07:05 +0300

    Test Program

--- Program.cs
+++ Program.cs
@@ -16,7 +16,7 @@ namespace Raven
         {   
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new SignIn());
+            Application.Run(new DesignForm());
         }
     }
 }
